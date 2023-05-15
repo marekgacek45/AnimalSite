b81155868e7ca0a045578af0f3574f9f0d87deb1
marekgacek45 <marekgacek45@gmail.com>
2023-05-15 18:41:42 +0200

    add users list

--- admin/users_list.php
+++ admin/users_list.php
@@ -2,9 +2,9 @@
 <?php Authentication::requireAdmin(); ?>
 <?php 
 
-$total = Animal::get_total($conn);
+$total = User::get_total($conn);
 $paginator = new Paginator($_GET['page'] ?? 1, 10, $total);
-$animals = Animal::get_page($conn, $paginator->limit, $paginator->offset);
+$users = User::get_page($conn, $paginator->limit, $paginator->offset);
 $base = strtok($_SERVER['REQUEST_URI'], "?");
 
 $i = 1;
@@ -18,35 +18,33 @@ $i = 1;
     </ul>
 
 
-    <a href="addAnimal.php"><button>dodaj</button></a>
 
     <table class="table table-striped text-center">
         <thead>
             <tr>
                 <th scope="col">#</th>
+                <th scope="col">Nazwa użytkownika</th>
                 <th scope="col">Imię</th>
-                <th scope="col">Zwierze</th>
-                <th scope="col">Skąd</th>
+                <th scope="col">Nazwisko</th>
+                <th scope="col">Email</th>
                 <th scope="col">Akcje</th>
             </tr>
         </thead>
         <tbody>
 
-            <?php foreach ($animals as $animal): ?>
+            <?php foreach ($users as $user): ?>
                 <tr>
                     <th scope="row">
                         <?= $i++ ?>
                     </th>
-                    <td><img src="../uploads/<?= $animal->image ?>" alt="zdjęcie <?= $animal->name ?>" width="30px"> <?= $animal->name ?></td>
-                    <td><?= $animal->type ?></td>
-                    <td><?= $animal->from_where ?></td>
+                    <td><img src="../uploads/<?= $user->image ?>" alt="zdjęcie <?= $user->username ?>" width="30px"> <?= $user->username ?></td>
+                    <td><?= $user->first_name ?></td>
+                    <td><?= $user->last_name ?></td>
+                    <td><?= $user->email ?></td>
                     <td>
-                        <a href="/AnimalSite/animal.php?id=<?= $animal->id ?>"> <button type="button"
+                        <a href="/AnimalSite/user.php?id=<?= $user->id ?>"> <button type="button"
                                 class="btn btn-primary">zobacz</button></a>
-                        <a href="/AnimalSite/admin/editAnimal.php?id=<?= $animal->id ?>"><button type="button"
-                                class="btn btn-primary">Edytuj</button></a>
-                        <a href="/AnimalSite/admin/deleteAnimal.php?id=<?= $animal->id ?>"><button type="button"
-                                class="btn btn-danger">Usuń</button></a>
+                        
 
                     </td>
 
