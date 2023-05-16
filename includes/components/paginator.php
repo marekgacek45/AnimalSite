938a94c12b6b6665ed58f8235411522062a3efaf
marekgacek45 <marekgacek45@gmail.com>
2023-05-16 21:05:12 +0200

    add some style

--- includes/components/paginator.php
+++ includes/components/paginator.php
@@ -1,28 +1,68 @@
-<nav>
-    <ul>
-      <li>
-        <?php if($paginator->previous): ?>
-          <a href="<?=$base?>?page=<?=$paginator->previous?>">Poprzednia</a>
-        <?php else: ?>
-          Poprzednia
-        <?php endif ?>
-      </li>
-      <li>
-        <?php if($paginator->next): ?>
-          <a href="<?=$base?>?page=<?=$paginator->next?>">Następna</a>
-        <?php else: ?>
-          Następna
-        <?php endif ?>
-      </li>
-    </ul>
-  </nav>
+<!-- <nav>
+  <ul>
+    <li>
+      <?php if ($paginator->previous): ?>
+        <a href="<?= $base ?>?page=<?= $paginator->previous ?>">Poprzednia</a>
+      <?php else: ?>
+        Poprzednia
+        <span class="page-link">Poprzednia</span>
+      <?php endif ?>
+    </li>
+    <li>
+      <?php if ($paginator->next): ?>
+        <a href="<?= $base ?>?page=<?= $paginator->next ?>">Następna</a>
+      <?php else: ?>
+        Następna
 
-  <nav>
-    <ul>
-      <?php for($i=1;$i<=$paginator->total_pages;$i++): ?>
-        <li>
-        <a href="<?=$base?>?page=<?=$i?>"><?=$i?></a>
-        </li>
-        <?php endfor ?>
-    </ul>
-  </nav>
+      <?php endif ?>
+    </li>
+  </ul>
+</nav>
+
+<nav>
+  <ul>
+    <?php for ($i = 1; $i <= $paginator->total_pages; $i++): ?>
+      <li>
+        <a href="<?= $base ?>?page=<?= $i ?>"><?= $i ?></a>
+      </li>
+    <?php endfor ?>
+  </ul>
+</nav> -->
+
+
+
+<nav aria-label="...">
+  <ul class="pagination">
+    <li class="page-item disabled">
+      <?php if ($paginator->previous): ?>
+        <a class="page-link" href="<?= $base ?>?page=<?= $paginator->previous ?>">Poprzednia</a>
+      <?php else: ?>
+        <span class="page-link">Poprzednia</span>
+      <?php endif ?>
+    </li>
+
+    <!-- <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item active" aria-current="page">
+      <span class="page-link">2</span>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li> -->
+
+    <?php for ($i = 1; $i <= $paginator->total_pages; $i++): ?>
+      <li>
+        <a class="page-link <?php  ?>" href="<?= $base ?>?page=<?= $i ?>"><?= $i ?></a>
+      </li>
+    <?php endfor ?>
+
+    <li class="page-item">
+      <?php if ($paginator->next): ?>
+        <a class="page-link" href="<?= $base ?>?page=<?= $paginator->next ?>">Następna</a>
+      <?php else: ?>
+        <span class="page-link">Następna</span>
+
+      <?php endif ?>
+
+
+
+    </li>
+  </ul>
+</nav>
--- index.php
+++ index.php
@@ -12,40 +12,28 @@ $base = strtok($_SERVER['REQUEST_URI'], "?")
 <main>
   <?php
 
-  $animalPhotos = Animal::getAll($conn);
-  $i = 1
+ $image_directory = "assets/header_images";
+ $images = glob($image_directory. '/*.jpg');
+ $i = 1;
     ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   <header class="container header">
     <div class="row d-flex justify-content-center align-items-center">
-      <div class="header__tex d-flex flex-column justify-content-center align-items-flex-start col-xs-12  col-lg-5">
+      <div class="header__text d-flex flex-column justify-content-center col-xs-12  col-lg-5">
         <h2 class="heading">Pomóż naszym przyjaciołom znaleźć swój nowy dom</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quod impedit repellendus harum atque similique
           voluptate ea fuga dolorem esse.</p>
-        <div>
-          <a href="#"><button class="btn btn-primary">Zobacz zwierzaki</button></a>
-          <a href="#"><button class="btn btn-primary">Napisz</button></a>
+        <div class="header__btns ">
+          <a href="#"><button class="btn custom-btn">Zobacz zwierzaki</button></a>
+          <a href="#"><button class="btn custom-btn">Napisz</button></a>
         </div>
       </div>
-      <div class="header__photos d-flex flex-wrap  col-xs-12  col-lg-7">
-        <?php foreach ($animalPhotos as $animal): ?>
-          <?php if ($i <= 16): ?>
-            <div class="header__photos-box">
+      <div class="header__photos d-flex flex-wrap justify-content-center align-items-center col-xs-12 col-lg-7">
+        <?php foreach ($images as $image): ?>
+          <?php if ($i <= 12): ?>
+            <div class="header__photos-box ">
 
-              <img src="uploads/<?= $animal->image ?>" alt="">
+              <img src="<?= $image ?>" alt="" loading="lazy">
               <?php $i++ ?>
             </div>
 
@@ -61,7 +49,7 @@ $base = strtok($_SERVER['REQUEST_URI'], "?")
 <section>
   <div class="d-flex justify-content-center align-items-center heroImg">
     <div class="heroImg-shadow"></div>
-    <div class="heroImg__content d-flex flex-column justify-content-center align-items-center">
+    <div class="heroImg__content d-flex flex-column justify-content-center align-items-center container">
       <h2 class="heading">Adoptuj Seniora</h2>
       <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
 <button class="btn btn-primary">sprawdź dobre pieski</button>
